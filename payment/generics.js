d22b22a85512a3d50880edd73bc2014190daf1e1
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-20 15:32:39 +0700

    [Feat]: Add Generics

---
+++ payment/generics.js
@@ -0,0 +1,34 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+/*
+const addUID = <T>(obj: T) => {
+   let uid = Math.floor(Math.random() * 100);
+   return {...obj, uid};
+ }
+*/
+var addUID = function (obj) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, obj), { uid: uid });
+};
+var doc1 = addUID({ name: 'adam', age: 40 });
+//let doc2 = addUID('Jack');
+console.log(doc1.name);
+var doc3 = {
+    uid: 1,
+    resourceName: 'person',
+    data: 'adam'
+};
+var doc4 = {
+    uid: 1,
+    resourceName: 'person',
+    data: ['cola', 'pepsi']
+};
---
+++ payment/generics.ts
@@ -0,0 +1,33 @@
+/*
+const addUID = <T>(obj: T) => {
+   let uid = Math.floor(Math.random() * 100);
+   return {...obj, uid};
+ }
+*/
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+ let doc1 = addUID({name: 'adam',age: 40});
+ //let doc2 = addUID('Jack');
+ console.log(doc1.name);
+
+
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const doc3: Resource<string> = {
+  uid: 1,
+  resourceName: 'person',
+  data: 'adam'
+};
+
+const doc4: Resource<string[]> = {
+  uid: 1,
+  resourceName: 'person',
+  data: ['cola','pepsi']
+};
