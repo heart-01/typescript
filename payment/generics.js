f249e728bbb7cfb97bf579bee2195899a6522cb7
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-20 17:04:22 +0700

    [Feat]: Add generic type examples to generics.js

--- payment/generics.js
+++ payment/generics.js
@@ -9,6 +9,7 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+// Example: Generic Type Object
 /*
 const addUID = <T>(obj: T) => {
    let uid = Math.floor(Math.random() * 100);
@@ -22,6 +23,44 @@ var addUID = function (obj) {
 var doc1 = addUID({ name: "adam", age: 40 });
 //let doc2 = addUID('Jack');
 console.log({ doc1: doc1 });
+// -----------------------------------------------
+// Example: Generic Type Array
+var reverse = function (arr) {
+    return arr.reverse();
+};
+var reversedNumber = reverse([1, 2, 3, 4, 5]);
+var reversedString = reverse(["a", "b", "c", "d", "e"]);
+console.log({ reversedNumber: reversedNumber });
+console.log({ reversedString: reversedString });
+// -----------------------------------------------
+// Example: Generic Type number | string
+var sum = function (a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return (a + b);
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return (a + b);
+    }
+    else {
+        throw new Error("Invalid argument types");
+    }
+};
+var result1 = sum(1, 2);
+var result2 = sum("Hello, ", "world");
+// -----------------------------------------------
+// Example: Generic Type And Name not sent, generic type
+var getFirst = function (arr) {
+    return arr[0];
+};
+var a = getFirst([1, 2, 3]); //  a: number เพราะ [1,2,3] เป็น number[] ทำให้ T = number
+var b = getFirst(["One", "Two", "Three", "Four", "Five"]); //  b: string เพราะ argument เป็น string[] ทำให้ T = string
+// -----------------------------------------------
+// Example: Generic Multiple Types
+var map = function (arr, func) {
+    return arr.map(func);
+};
+var input = ["4", "5", "6"];
+map(input, function (s) { return parseInt(s); }); // [4, 5, 6]
 var doc3 = {
     uid: 1,
     resourceName: "person",
@@ -32,3 +71,49 @@ var doc4 = {
     resourceName: "person",
     data: ["cola", "pepsi"],
 };
+// -----------------------------------------------
+// Example: Generic Types In Class
+var Box = /** @class */ (function () {
+    function Box(value) {
+        this.content = value;
+    }
+    Box.prototype.getValue = function () {
+        return this.content;
+    };
+    return Box;
+}());
+var numberBox = new Box(42);
+var stringBox = new Box("Hello generics!");
+console.log({ numberBox: numberBox });
+console.log({ stringBox: stringBox });
+// -----------------------------------------------
+// Example: Generic Types In Class
+var Box = /** @class */ (function () {
+    function Box(value) {
+        this.content = value;
+    }
+    Box.prototype.getValue = function () {
+        return this.content;
+    };
+    return Box;
+}());
+var numberBox = new Box(42);
+var stringBox = new Box("Hello generics!");
+console.log({ numberBox: numberBox });
+console.log({ stringBox: stringBox });
+// -----------------------------------------------
+// Example: Generic Multiple Types In Class
+var Box2 = /** @class */ (function () {
+    function Box2(content, content2, content3) {
+        this.content = content;
+        this.content2 = content2;
+        this.content3 = content3;
+    }
+    Box2.prototype.getValue = function () {
+        console.log(this.content);
+        console.log(this.content2);
+        console.log(this.content3);
+    };
+    return Box2;
+}());
+var testBox2 = new Box2(42, "Hello", true);
--- payment/generics.ts
+++ payment/generics.ts
@@ -1,3 +1,4 @@
+// Example: Generic Type Object
 /*
 const addUID = <T>(obj: T) => {
    let uid = Math.floor(Math.random() * 100);
@@ -11,8 +12,55 @@ const addUID = <T extends { name: string }>(obj: T): T => {
 
 let doc1 = addUID({ name: "adam", age: 40 });
 //let doc2 = addUID('Jack');
-console.log({doc1});
+console.log({ doc1 });
 
+// -----------------------------------------------
+// Example: Generic Type Array
+const reverse = <T>(arr: T[]): T[] => {
+  return arr.reverse();
+};
+
+const reversedNumber = reverse<number>([1, 2, 3, 4, 5]);
+const reversedString = reverse<string>(["a", "b", "c", "d", "e"]);
+
+console.log({ reversedNumber });
+console.log({ reversedString });
+
+// -----------------------------------------------
+// Example: Generic Type number | string
+const sum = <T extends number | string>(a: T, b: T): T => {
+  if (typeof a === "number" && typeof b === "number") {
+    return (a + b) as T;
+  } else if (typeof a === "string" && typeof b === "string") {
+    return (a + b) as T;
+  } else {
+    throw new Error("Invalid argument types");
+  }
+};
+
+const result1 = sum(1, 2);
+const result2 = sum("Hello, ", "world");
+
+// -----------------------------------------------
+// Example: Generic Type And Name not sent, generic type
+const getFirst = <T>(arr: T[]): T => {
+  return arr[0];
+};
+
+const a = getFirst([1, 2, 3]); //  a: number เพราะ [1,2,3] เป็น number[] ทำให้ T = number
+const b = getFirst(["One", "Two", "Three", "Four", "Five"]); //  b: string เพราะ argument เป็น string[] ทำให้ T = string
+
+// -----------------------------------------------
+// Example: Generic Multiple Types
+const map = <T, U>(arr: T[], func: (arg: T) => U): U[] => {
+  return arr.map(func);
+};
+
+const input = ["4", "5", "6"];
+map(input, (s) => parseInt(s)); // [4, 5, 6]
+
+// -----------------------------------------------
+// Example: Generic Types In Interface
 interface Resource<T> {
   uid: number;
   resourceName: string;
@@ -30,3 +78,57 @@ const doc4: Resource<string[]> = {
   resourceName: "person",
   data: ["cola", "pepsi"],
 };
+
+// -----------------------------------------------
+// Example: Generic Types In Class
+class Box<T> {
+  private content: T;
+
+  constructor(value: T) {
+    this.content = value;
+  }
+
+  getValue(): T {
+    return this.content;
+  }
+}
+
+const numberBox = new Box<number>(42);
+const stringBox = new Box<string>("Hello generics!");
+
+console.log({ numberBox });
+console.log({ stringBox });
+
+// -----------------------------------------------
+// Example: Generic Types In Class
+class Box<T> {
+  private content: T;
+
+  constructor(value: T) {
+    this.content = value;
+  }
+
+  getValue(): T {
+    return this.content;
+  }
+}
+
+const numberBox = new Box<number>(42);
+const stringBox = new Box<string>("Hello generics!");
+
+console.log({ numberBox });
+console.log({ stringBox });
+
+// -----------------------------------------------
+// Example: Generic Multiple Types In Class
+class Box2<T, U, H> {
+  constructor(private content: T, private content2: U, private content3: H) {}
+
+  getValue(): void {
+    console.log(this.content);
+    console.log(this.content2);
+    console.log(this.content3);
+  }
+}
+
+const testBox2 = new Box2<number, string, boolean>(42, "Hello", true);
